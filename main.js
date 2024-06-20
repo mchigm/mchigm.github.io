f372794cf1cdb717192d445561e7cc2e43965b85
MCHIGM <yuemingdd@gmail.com>
2024-06-20 19:54:05 +0000

    Update 2

---
+++ main.js
@@ -0,0 +1,9 @@
+function copyDel() {
+    navigator.clipboard.writeText("0085269981753");
+}
+function copyEmaila() {
+	navigator.clipboard.writeText("yuemingdd@gmail.com")
+}
+function copyEmaila() {
+	navigator.clipboard.writeText("201806006@stu.pkc.edu.hk")
+}
